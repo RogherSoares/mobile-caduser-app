ad0ee060f452d2cf1914b96f43eee9ba699b7aa8
Rogher Adriano Soares <rogher_adriano@hotmail.com>
2026-03-19 21:28:22 -0300

    arquivos iniciais do app

---
+++ app/src/main/java/com/roghersoares/cadastrodeusuario/CreateUser.java
@@ -0,0 +1,13 @@
+package com.roghersoares.cadastrodeusuario;
+
+//bloco de importações
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.textfield.TextInputEditText;
+
+
+//classe principal
+public class CreateUser  extends AppCompatActivity {
+    textInputEditText editNome, editEmail;
+    Button btnSalvar;
+}
---
+++ app/src/main/java/com/roghersoares/cadastrodeusuario/UserAdapter.java
@@ -0,0 +1,4 @@
+package com.roghersoares.cadastrodeusuario;
+
+public class UserAdapter {
+}
