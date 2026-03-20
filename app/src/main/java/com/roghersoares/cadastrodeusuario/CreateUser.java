4f6ef27eadf9a0a7288fa1cbc17155afa3e18ba6
Rogher Adriano Soares <rogher_adriano@hotmail.com>
2026-03-19 21:51:42 -0300

    incio da logica do createuser

--- app/src/main/java/com/roghersoares/cadastrodeusuario/CreateUser.java
+++ app/src/main/java/com/roghersoares/cadastrodeusuario/CreateUser.java
@@ -1,6 +1,7 @@
 package com.roghersoares.cadastrodeusuario;
 
 //bloco de importações
+import android.os.Bundle;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.textfield.TextInputEditText;
@@ -10,4 +11,25 @@ import com.google.android.material.textfield.TextInputEditText;
 public class CreateUser  extends AppCompatActivity {
     textInputEditText editNome, editEmail;
     Button btnSalvar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_user);
+
+        //vincula variavel com elemento de interface gráfica, escrito no arquivo xml
+        editNome = findViewById(R.id.nome_completo);
+        editEmail = findViewById(R.id.email);
+        btnSalvar = findViewById(R.id.btnSalvar);
+
+        btnSalvar.setOnClickListener(v -> {
+            String nome = editNome.getText().toString();
+            String email = editEmail.getText().toString();
+
+            if(!nome.isEmpty()){
+                MainActivity.listaNomes.add(nome +"(" + email +")");
+                finish();
+            }
+        });
+    }
 }
